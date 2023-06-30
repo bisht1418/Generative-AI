9331f6848da736a36d6b3329072322fda175d8e0
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-06-30 20:22:45 +0530

    alternative gpt solution

--- S_01/D_04/Set_03/sum_problem.py
+++ S_01/D_04/Set_03/sum_problem.py
@@ -10,3 +10,25 @@ for x in range(0, len(list)):
 
         if sum == target:
             print([x, y])
+
+
+# ==================================================
+# GPT solution
+
+# def two_sum(nums, target):
+#     num_map = {}  # HashMap to store values and indices
+
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in num_map:
+#             return [num_map[complement], i]
+#         num_map[num] = i
+
+#     return []
+
+
+# # Example usage:
+# nums = [2, 7, 11, 15]
+# target = 9
+# result = two_sum(nums, target)
+# print(result)
