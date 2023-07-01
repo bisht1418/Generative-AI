631da8551759926749d6bbea03a6c60a7d40764f
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-07-01 09:20:26 +0530

    update the code

--- S_01/D_05/Live_Project/Mumbai_Munchies/snack_inventory.py
+++ S_01/D_05/Live_Project/Mumbai_Munchies/snack_inventory.py
@@ -1,61 +1,101 @@
 class Snack:
-    def __init__(self, snack_id, name, price, availability):
-        self.snack_id = snack_id
+    def __init__(self, id, name, price, availability, quantity):
+        self.id = id
         self.name = name
         self.price = price
         self.availability = availability
+        self.quantity = quantity
 
 
 class SnackInventory:
     def __init__(self):
-        self.inventory = []
+        self.snacks = []
 
     def add_snack(self, snack):
-        self.inventory.append(snack)
+        self.snacks.append(snack)
 
     def remove_snack(self, snack_id):
-        for snack in self.inventory:
-            if snack.snack_id == snack_id:
-                self.inventory.remove(snack)
+        for snack in self.snacks:
+            if snack.id == snack_id:
+                self.snacks.remove(snack)
                 break
 
     def update_availability(self, snack_id, availability):
-        for snack in self.inventory:
-            if snack.snack_id == snack_id:
+        for snack in self.snacks:
+            if snack.id == snack_id:
                 snack.availability = availability
                 break
 
     def sell_snack(self, snack_id):
-        for snack in self.inventory:
-            if snack.snack_id == snack_id:
+        for snack in self.snacks:
+            if snack.id == snack_id:
                 if snack.availability == "yes":
-                    snack.availability = "no"
+                    snack.quantity -= 1
                     return True
                 else:
-                    print("Snack is not available.")
+                    print("Snack is not available for sale.")
                     return False
         print("Snack not found.")
         return False
 
+    def get_all_snacks(self):
+        return self.snacks
+
 
 def display_menu():
-    print("1. Add snack")
-    print("2. Remove snack")
-    print("3. Update snack availability")
-    print("4. Sell snack")
-    print("5. Exit")
+    print("1. Add a snack to the inventory")
+    print("2. Remove a snack from the inventory")
+    print("3. Update the availability of a snack")
+    print("4. Sell a snack")
+    print("5. List all snacks")
+    print("6. Exit")
 
 
 def get_snack_details():
     snack_id = input("Enter snack ID: ")
     name = input("Enter snack name: ")
-    price = input("Enter snack price: ")
-    availability = input("Enter snack availability (yes/no): ")
-    return Snack(snack_id, name, price, availability)
+    price = float(input("Enter snack price: "))
+    availability = input("Is the snack available? (yes/no): ")
+    quantity = int(input("Enter snack quantity: "))
+    return Snack(snack_id, name, price, availability, quantity)
 
 
-def get_snack_id():
-    return input("Enter snack ID: ")
+def add_snack_to_inventory(inventory):
+    snack = get_snack_details()
+    inventory.add_snack(snack)
+    print("Snack added to the inventory.")
+
+
+def remove_snack_from_inventory(inventory):
+    snack_id = input("Enter snack ID to remove: ")
+    inventory.remove_snack(snack_id)
+    print("Snack removed from the inventory.")
+
+
+def update_snack_availability(inventory):
+    snack_id = input("Enter snack ID to update availability: ")
+    availability = input("Is the snack available now? (yes/no): ")
+    inventory.update_availability(snack_id, availability)
+    print("Snack availability updated.")
+
+
+def sell_snack(inventory):
+    snack_id = input("Enter snack ID to sell: ")
+    if inventory.sell_snack(snack_id):
+        print("Snack sold. Inventory updated.")
+    else:
+        print("Snack sale unsuccessful.")
+
+
+def list_all_snacks(inventory):
+    all_snacks = inventory.get_all_snacks()
+    if all_snacks:
+        print("List of snacks:")
+        for snack in all_snacks:
+            print(
+                f"ID: {snack.id}, Name: {snack.name}, Quantity: {snack.quantity}")
+    else:
+        print("No snacks in the inventory.")
 
 
 def main():
@@ -66,24 +106,17 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            snack = get_snack_details()
-            inventory.add_snack(snack)
-            print("Snack added to inventory.")
+            add_snack_to_inventory(inventory)
         elif choice == "2":
-            snack_id = get_snack_id()
-            inventory.remove_snack(snack_id)
-            print("Snack removed from inventory.")
+            remove_snack_from_inventory(inventory)
         elif choice == "3":
-            snack_id = get_snack_id()
-            availability = input("Enter new availability (yes/no): ")
-            inventory.update_availability(snack_id, availability)
-            print("Snack availability updated.")
+            update_snack_availability(inventory)
         elif choice == "4":
-            snack_id = get_snack_id()
-            if inventory.sell_snack(snack_id):
-                print("Snack sold.")
+            sell_snack(inventory)
         elif choice == "5":
-            print("Exiting program.")
+            list_all_snacks(inventory)
+        elif choice == "6":
+            print("Exiting the program.")
             break
         else:
             print("Invalid choice. Please try again.")
