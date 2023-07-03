11ed050e8da73aaa0f062aa1c20892ab20e64f21
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-07-03 08:12:08 +0530

    todo

---
+++ S_02/D_01/Training/Todo/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+todos = []
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form.get('todo')
+    if todo:
+        todos.append(todo)
+    return redirect('/')
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    todo = request.form.get('todo')
+    if todo in todos:
+        todos.remove(todo)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
