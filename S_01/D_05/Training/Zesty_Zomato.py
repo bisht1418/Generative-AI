f03c3ffe02cac7f31c37e04d0baed389c9ff4a7b
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-07-03 12:36:16 +0530

    Zest_Zomato finally completed

--- S_01/D_05/Training/Zesty_Zomato.py
+++ S_01/D_05/Training/Zesty_Zomato.py
@@ -2,118 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-class ZestyZomatoApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Zesty Zomato")
-        self.menu_frame = tk.Frame(root)
-        self.menu_frame.pack(padx=20, pady=20)
-
-        self.dish_id_label = tk.Label(self.menu_frame, text="Dish ID:")
-        self.dish_id_label.grid(row=0, column=0)
-        self.dish_id_entry = tk.Entry(self.menu_frame)
-        self.dish_id_entry.grid(row=0, column=1)
-
-        self.name_label = tk.Label(self.menu_frame, text="Dish Name:")
-        self.name_label.grid(row=1, column=0)
-        self.name_entry = tk.Entry(self.menu_frame)
-        self.name_entry.grid(row=1, column=1)
-
-        self.price_label = tk.Label(self.menu_frame, text="Price:")
-        self.price_label.grid(row=2, column=0)
-        self.price_entry = tk.Entry(self.menu_frame)
-        self.price_entry.grid(row=2, column=1)
-
-        self.availability_label = tk.Label(
-            self.menu_frame, text="Availability:")
-        self.availability_label.grid(row=3, column=0)
-        self.availability_entry = tk.Entry(self.menu_frame)
-        self.availability_entry.grid(row=3, column=1)
-
-        self.add_dish_button = tk.Button(
-            self.menu_frame, text="Add Dish", command=self.add_dish)
-        self.add_dish_button.grid(row=4, columnspan=2, pady=10)
-
-        self.buy_frame = tk.Frame(root)
-        self.buy_frame.pack(padx=20, pady=20)
-
-        self.customer_name_label = tk.Label(
-            self.buy_frame, text="Customer Name:")
-        self.customer_name_label.grid(row=0, column=0)
-        self.customer_name_entry = tk.Entry(self.buy_frame)
-        self.customer_name_entry.grid(row=0, column=1)
-
-        self.order_label = tk.Label(self.buy_frame, text="Order (Dish IDs):")
-        self.order_label.grid(row=1, column=0)
-        self.order_entry = tk.Entry(self.buy_frame)
-        self.order_entry.grid(row=1, column=1)
-
-        self.buy_button = tk.Button(
-            self.buy_frame, text="Buy", command=self.buy_dishes)
-        self.buy_button.grid(row=2, columnspan=2, pady=10)
-
-        self.review_frame = tk.Frame(root)
-        self.review_frame.pack(padx=20, pady=20)
-
-        self.review_button = tk.Button(
-            self.review_frame, text="Review Orders", command=self.review_orders)
-        self.review_button.pack(pady=10)
-
-        self.order_id_label = tk.Label(self.review_frame, text="Order ID:")
-        self.order_id_label.pack()
-        self.order_id_entry = tk.Entry(self.review_frame)
-        self.order_id_entry.pack()
-
-        self.status_label = tk.Label(self.review_frame, text="Status:")
-        self.status_label.pack()
-        self.status_entry = tk.Entry(self.review_frame)
-        self.status_entry.pack()
-
-        self.update_status_button = tk.Button(
-            self.review_frame, text="Update Status", command=self.update_status)
-        self.update_status_button.pack(pady=10)
-
-    def add_dish(self):
-        dish_id = self.dish_id_entry.get()
-        name = self.name_entry.get()
-        price = self.price_entry.get()
-        availability = self.availability_entry.get()
-
-        # Perform validation and add the dish to the menu
-        # ...
-
-        messagebox.showinfo("Success", "Dish added to the menu.")
-
-    def buy_dishes(self):
-        customer_name = self.customer_name_entry.get()
-        order = self.order_entry.get()
-
-        # Perform validation and process the order
-        # ...
-
-        messagebox.showinfo("Success", "Order placed successfully.")
-
-    def review_orders(self):
-        # Retrieve and display all orders
-        # ...
-
-        messagebox.showinfo("Orders", "Display all orders here.")
-
-    def update_status(self):
-        order_id = self.order_id_entry.get()
-        status = self.status_entry.get()
-
-        # Perform validation and update the order status
-        # ...
-
-        messagebox.showinfo("Success", "Order status updated.")
-
-
-root = tk.Tk()
-app = ZestyZomatoApp(root)
-root.mainloop()
-
-
 class Dish:
     def __init__(self, dish_id, name, price, availability):
         self.dish_id = dish_id
@@ -122,113 +10,267 @@ class Dish:
         self.availability = availability
 
 
+class ZestyZomato:
+    def __init__(self):
+        self.menu = []
+        self.orders = []
+        self.order_id = 1
+
+    def add_dish(self, dish):
+        self.menu.append(dish)
+
+    def remove_dish(self, dish_id):
+        for dish in self.menu:
+            if dish.dish_id == dish_id:
+                self.menu.remove(dish)
+                return True
+        return False
+
+    def update_availability(self, dish_id, availability):
+        for dish in self.menu:
+            if dish.dish_id == dish_id:
+                dish.availability = availability
+                return True
+        return False
+
+    def take_order(self, customer_name, dish_ids):
+        dishes_ordered = []
+        for dish_id in dish_ids:
+            for dish in self.menu:
+                if dish.dish_id == dish_id:
+                    if dish.availability == "yes":
+                        dishes_ordered.append(dish)
+                    else:
+                        messagebox.showinfo(
+                            "Error", f"{dish.name} is not available for order.")
+                        return
+        order = Order(self.order_id, customer_name, dishes_ordered)
+        self.orders.append(order)
+        self.order_id += 1
+        messagebox.showinfo(
+            "Success", f"Order taken successfully. Order ID: {order.order_id}")
+
+    def update_status(self, order_id, status):
+        for order in self.orders:
+            if order.order_id == order_id:
+                order.status = status
+                messagebox.showinfo(
+                    "Success", f"Order status updated. Order ID: {order.order_id}")
+                return
+        messagebox.showinfo("Error", "Invalid Order ID")
+
+
 class Order:
-    def __init__(self, order_id, customer_name, dish_ids):
+    def __init__(self, order_id, customer_name, dishes_ordered):
         self.order_id = order_id
         self.customer_name = customer_name
-        self.dish_ids = dish_ids
+        self.dishes_ordered = dishes_ordered
         self.status = "received"
 
 
-class ZestyZomato:
+class ZestyZomatoGUI:
     def __init__(self):
-        self.menu = {}
-        self.orders = {}
-        self.next_order_id = 1
+        self.zesty_zomato = ZestyZomato()
 
-    def add_dish(self, dish):
-        self.menu[dish.dish_id] = dish
+        self.root = tk.Tk()
+        self.root.title("Zesty Zomato")
 
-    def remove_dish(self, dish_id):
-        if dish_id in self.menu:
-            del self.menu[dish_id]
+        self.create_menu_frame()
+        self.create_orders_frame()
+        self.create_buttons()
 
-    def update_dish_availability(self, dish_id, availability):
-        if dish_id in self.menu:
-            self.menu[dish_id].availability = availability
+    def create_menu_frame(self):
+        self.menu_frame = tk.LabelFrame(self.root, text="Menu")
+        self.menu_frame.pack(padx=10, pady=10, fill=tk.BOTH)
 
-    def take_order(self, customer_name, dish_ids):
-        available_dish_ids = [
-            dish.dish_id for dish in self.menu.values() if dish.availability == "yes"]
-        for dish_id in dish_ids:
-            if dish_id not in available_dish_ids:
-                print(f"Dish with ID {dish_id} is not available.")
-                return
+        self.menu_listbox = tk.Listbox(self.menu_frame, width=50)
+        self.menu_listbox.pack(padx=10, pady=10)
 
-        order = Order(self.next_order_id, customer_name, dish_ids)
-        self.orders[self.next_order_id] = order
-        self.next_order_id += 1
-        print(f"Order placed successfully. Order ID: {order.order_id}")
+    def update_menu_listbox(self):
+        self.menu_listbox.delete(0, tk.END)
+        for dish in self.zesty_zomato.menu:
+            self.menu_listbox.insert(
+                tk.END, f"{dish.dish_id} - {dish.name} - Rs. {dish.price}")
 
-    def update_order_status(self, order_id, new_status):
-        if order_id in self.orders:
-            self.orders[order_id].status = new_status
+    def create_orders_frame(self):
+        self.orders_frame = tk.LabelFrame(self.root, text="Orders")
+        self.orders_frame.pack(padx=10, pady=10, fill=tk.BOTH)
+
+        self.orders_listbox = tk.Listbox(self.orders_frame, width=50)
+        self.orders_listbox.pack(padx=10, pady=10)
+
+    def update_orders_listbox(self):
+        self.orders_listbox.delete(0, tk.END)
+        for order in self.zesty_zomato.orders:
+            self.orders_listbox.insert(
+                tk.END, f"Order ID: {order.order_id} - Customer: {order.customer_name} - Status: {order.status}")
+
+    def create_buttons(self):
+        add_dish_button = tk.Button(
+            self.root, text="Add Dish", command=self.open_add_dish_window)
+        add_dish_button.pack(pady=10)
+
+        remove_dish_button = tk.Button(
+            self.root, text="Remove Dish", command=self.open_remove_dish_window)
+        remove_dish_button.pack(pady=10)
+
+        update_availability_button = tk.Button(
+            self.root, text="Update Availability", command=self.open_update_availability_window)
+        update_availability_button.pack(pady=10)
+
+        take_order_button = tk.Button(
+            self.root, text="Take Order", command=self.open_take_order_window)
+        take_order_button.pack(pady=10)
+
+        update_status_button = tk.Button(
+            self.root, text="Update Status", command=self.open_update_status_window)
+        update_status_button.pack(pady=10)
+
+    def open_add_dish_window(self):
+        add_dish_window = tk.Toplevel(self.root)
+        add_dish_window.title("Add Dish")
+
+        dish_id_label = tk.Label(add_dish_window, text="Dish ID:")
+        dish_id_label.grid(row=0, column=0, padx=5, pady=5)
+        dish_id_entry = tk.Entry(add_dish_window)
+        dish_id_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        dish_name_label = tk.Label(add_dish_window, text="Dish Name:")
+        dish_name_label.grid(row=1, column=0, padx=5, pady=5)
+        dish_name_entry = tk.Entry(add_dish_window)
+        dish_name_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        price_label = tk.Label(add_dish_window, text="Price:")
+        price_label.grid(row=2, column=0, padx=5, pady=5)
+        price_entry = tk.Entry(add_dish_window)
+        price_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        availability_label = tk.Label(add_dish_window, text="Availability:")
+        availability_label.grid(row=3, column=0, padx=5, pady=5)
+        availability_entry = tk.Entry(add_dish_window)
+        availability_entry.grid(row=3, column=1, padx=5, pady=5)
+
+        add_button = tk.Button(add_dish_window, text="Add", command=lambda: self.add_dish(
+            add_dish_window, dish_id_entry.get(), dish_name_entry.get(), price_entry.get(), availability_entry.get()))
+        add_button.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+
+    def add_dish(self, add_dish_window, dish_id, dish_name, price, availability):
+        try:
+            price = float(price)
+            dish = Dish(dish_id, dish_name, price, availability)
+            self.zesty_zomato.add_dish(dish)
+            self.update_menu_listbox()
+            messagebox.showinfo("Success", "Dish added successfully.")
+            add_dish_window.destroy()
+        except ValueError:
+            messagebox.showinfo(
+                "Error", "Invalid price. Please enter a numeric value.")
+
+    def open_remove_dish_window(self):
+        remove_dish_window = tk.Toplevel(self.root)
+        remove_dish_window.title("Remove Dish")
+
+        dish_id_label = tk.Label(remove_dish_window, text="Dish ID:")
+        dish_id_label.grid(row=0, column=0, padx=5, pady=5)
+        dish_id_entry = tk.Entry(remove_dish_window)
+        dish_id_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        remove_button = tk.Button(remove_dish_window, text="Remove", command=lambda: self.remove_dish(
+            remove_dish_window, dish_id_entry.get()))
+        remove_button.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+
+    def remove_dish(self, remove_dish_window, dish_id):
+        if self.zesty_zomato.remove_dish(dish_id):
+            self.update_menu_listbox()
+            messagebox.showinfo("Success", "Dish removed successfully.")
+            remove_dish_window.destroy()
         else:
-            print("Order not found.")
+            messagebox.showinfo("Error", "Invalid Dish ID.")
 
-    def review_orders(self):
-        if self.orders:
-            for order in self.orders.values():
-                print(
-                    f"Order ID: {order.order_id}, Customer Name: {order.customer_name}, Status: {order.status}")
+    def open_update_availability_window(self):
+        update_availability_window = tk.Toplevel(self.root)
+        update_availability_window.title("Update Availability")
+
+        dish_id_label = tk.Label(update_availability_window, text="Dish ID:")
+        dish_id_label.grid(row=0, column=0, padx=5, pady=5)
+        dish_id_entry = tk.Entry(update_availability_window)
+        dish_id_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        availability_label = tk.Label(
+            update_availability_window, text="Availability:")
+        availability_label.grid(row=1, column=0, padx=5, pady=5)
+        availability_entry = tk.Entry(update_availability_window)
+        availability_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        update_button = tk.Button(update_availability_window, text="Update", command=lambda: self.update_availability(
+            update_availability_window, dish_id_entry.get(), availability_entry.get()))
+        update_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+    def update_availability(self, update_availability_window, dish_id, availability):
+        if self.zesty_zomato.update_availability(dish_id, availability):
+            self.update_menu_listbox()
+            messagebox.showinfo(
+                "Success", "Availability updated successfully.")
+            update_availability_window.destroy()
         else:
-            print("No orders to review.")
+            messagebox.showinfo("Error", "Invalid Dish ID.")
 
+    def open_take_order_window(self):
+        take_order_window = tk.Toplevel(self.root)
+        take_order_window.title("Take Order")
 
-def display_menu():
-    print("1. Add a dish to the menu")
-    print("2. Remove a dish from the menu")
-    print("3. Update the availability of a dish")
-    print("4. Take a new order")
-    print("5. Update order status")
-    print("6. Review orders")
-    print("7. Exit")
+        customer_name_label = tk.Label(
+            take_order_window, text="Customer Name:")
+        customer_name_label.grid(row=0, column=0, padx=5, pady=5)
+        customer_name_entry = tk.Entry(take_order_window)
+        customer_name_entry.grid(row=0, column=1, padx=5, pady=5)
 
+        dish_ids_label = tk.Label(
+            take_order_window, text="Dish IDs (comma-separated):")
+        dish_ids_label.grid(row=1, column=0, padx=5, pady=5)
+        dish_ids_entry = tk.Entry(take_order_window)
+        dish_ids_entry.grid(row=1, column=1, padx=5, pady=5)
 
-def get_dish_details():
-    dish_id = input("Enter dish ID: ")
-    name = input("Enter dish name: ")
-    price = float(input("Enter dish price: "))
-    availability = input("Is the dish available? (yes/no): ")
-    return Dish(dish_id, name, price, availability)
+        take_button = tk.Button(take_order_window, text="Take Order", command=lambda: self.take_order(
+            take_order_window, customer_name_entry.get(), dish_ids_entry.get()))
+        take_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
+    def take_order(self, take_order_window, customer_name, dish_ids):
+        dish_ids = dish_ids.split(",")
+        dish_ids = [dish_id.strip() for dish_id in dish_ids]
+        self.zesty_zomato.take_order(customer_name, dish_ids)
+        self.update_orders_listbox()
+        take_order_window.destroy()
 
-def main():
-    zesty_zomato = ZestyZomato()
+    def open_update_status_window(self):
+        update_status_window = tk.Toplevel(self.root)
+        update_status_window.title("Update Status")
 
-    while True:
-        display_menu()
-        choice = input("Enter your choice: ")
+        order_id_label = tk.Label(update_status_window, text="Order ID:")
+        order_id_label.grid(row=0, column=0, padx=5, pady=5)
+        order_id_entry = tk.Entry(update_status_window)
+        order_id_entry.grid(row=0, column=1, padx=5, pady=5)
 
-        if choice == "1":
-            dish = get_dish_details()
-            zesty_zomato.add_dish(dish)
-            print("Dish added to the menu.")
-        elif choice == "2":
-            dish_id = input("Enter dish ID to remove: ")
-            zesty_zomato.remove_dish(dish_id)
-            print("Dish removed from the menu.")
-        elif choice == "3":
-            dish_id = input("Enter dish ID to update availability: ")
-            availability = input("Enter new availability (yes/no): ")
-            zesty_zomato.update_dish_availability(dish_id, availability)
-            print("Dish availability updated.")
-        elif choice == "4":
-            customer_name = input("Enter customer name: ")
-            dish_ids = input("Enter dish IDs (comma-separated): ").split(",")
-            zesty_zomato.take_order(customer_name, dish_ids)
-        elif choice == "5":
-            order_id = int(input("Enter order ID to update status: "))
-            new_status = input("Enter new status: ")
-            zesty_zomato.update_order_status(order_id, new_status)
-            print("Order status updated.")
-        elif choice == "6":
-            zesty_zomato.review_orders()
-        elif choice == "7":
-            break
-        else:
-            print("Invalid choice. Please try again.")
+        status_label = tk.Label(update_status_window, text="Status:")
+        status_label.grid(row=1, column=0, padx=5, pady=5)
+        status_entry = tk.Entry(update_status_window)
+        status_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        update_button = tk.Button(update_status_window, text="Update", command=lambda: self.update_status(
+            update_status_window, order_id_entry.get(), status_entry.get()))
+        update_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+    def update_status(self, update_status_window, order_id, status):
+        self.zesty_zomato.update_status(order_id, status)
+        self.update_orders_listbox()
+        update_status_window.destroy()
+
+    def run(self):
+        self.update_menu_listbox()
+        self.update_orders_listbox()
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    gui = ZestyZomatoGUI()
+    gui.run()
