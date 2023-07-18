127cd7bb68e7f0511b214359e5cc57b54272c769
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-07-19 01:13:59 +0530

    update register and login functionality

--- Project/MovieManiaBackend/app.py
+++ Project/MovieManiaBackend/app.py
@@ -1,90 +1,114 @@
-from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
-from pymongo import MongoClient
 from bson import ObjectId
+from flask import jsonify
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_marshmallow import Marshmallow
+from bson.objectid import ObjectId
+from mongoengine import Document, StringField, ReferenceField, DateTimeField, ObjectIdField, connect
 
-# Initialize Flask app and API
+
 app = Flask(__name__)
-api = Api(app)
+app.config['MONGO_URI'] = 'mongodb+srv://bishtnrj1418:neeraj@cluster0.ofkqsxc.mongodb.net/MovieMania?retryWrites=true&w=majority'
+# Change this to a random secure key in production
+app.config['JWT_SECRET_KEY'] = 'neeraj'
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb+srv://bishtnrj1418:neeraj@cluster0.ofkqsxc.mongodb.net/MovieMania?retryWrites=true&w=majority'
+}
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+ma = Marshmallow(app)
 
-# Set up MongoDB connection
-client = MongoClient(
-    "mongodb+srv://bishtnrj1418:neeraj@cluster0.ofkqsxc.mongodb.net/?retryWrites=true&w=majority")
-# Replace "cine_matrix" with your desired database name
-db = client["MovieMania"]
-users_collection = db["users"]
-
-
-def is_email_taken(email):
-    return users_collection.find_one({"email": email}) is not None
-
-# Helper function to get user data by username
+# Define a function to connect to MongoDB using the MONGODB_SETTINGS
 
 
-def get_user_by_email(email):
-    return users_collection.find_one({"email": email})
-
-# Resource for user registration
+def initialize_db(app):
+    connect(host=app.config['MONGODB_SETTINGS']['host'])
 
 
-class UserRegistration(Resource):
-    def post(self):
-        data = request.get_json()
+initialize_db(app)
 
-        # Check if the required fields are present in the request
-        required_fields = ["username", "password",
-                           "gender", "membershipType", "dateOfBirth", "email"]
-        for field in required_fields:
-            if field not in data:
-                return {"message": f"Missing required field: {field}"}, 400
-
-        # Check if the username is already taken
-        if is_email_taken(data["email"]):
-            return {"message": "Username already taken"}, 400
-
-        # Save the user data to the database
-        user_data = {
-            "username": data["username"],
-            # In a real application, we would hash the password
-            "password": data["password"],
-            "gender": data["gender"],
-            "membershipType": data["membershipType"],
-            "dateOfBirth": data["dateOfBirth"],
-            "email": data["email"]
-        }
-        users_collection.insert_one(user_data)
-
-        return {"message": "User registered successfully"}, 201
-
-# Resource for user login
+# Define data models for Users, Movies, Shows, Events, and Participants
 
 
-class UserLogin(Resource):
-    def post(self):
-        data = request.get_json()
-
-        # Check if the required fields are present in the request
-        required_fields = ["email", "password"]
-        for field in required_fields:
-            if field not in data:
-                return {"message": f"Missing required field: {field}"}, 400
-
-        # Find the user by email
-        user = get_user_by_email(data["email"])
-
-        # Check if the user exists and the password matches (in a real application, we would compare hashed passwords)
-        if not user or user["password"] != data["password"]:
-            return {"message": "Invalid credentials"}, 401
-
-        # Convert the ObjectId to a string
-        user["_id"] = str(user["_id"])
-
-        return {"user": user, "token": "123"}, 200
+class User(Document):
+    username = StringField(required=True)
+    email = StringField(required=True)
+    password = StringField(required=True)
+    role = StringField(default='regular')
 
 
-# Add API resources to routes
-api.add_resource(UserRegistration, "/api/register")
-api.add_resource(UserLogin, "/api/login")
+class Movie(Document):
+    title = StringField(required=True)
+    description = StringField(required=True)
+    release_date = StringField(required=True)
+    duration = StringField(required=True)
+    image_url = StringField()
 
-if __name__ == "__main__":
+
+class Show(Document):
+    movie_id = ObjectIdField(required=True)
+    show_time = StringField(required=True)
+    category = StringField()
+
+
+class Event(Document):
+    name = StringField(required=True)
+    description = StringField(required=True)
+    event_date = StringField(required=True)
+    venue = StringField(required=True)
+
+
+class Participant(Document):
+    user_id = ObjectIdField(required=True)
+    event_id = ObjectIdField(required=True)
+
+
+# Define a Marshmallow schema for the User data model
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'username', 'email', 'role')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    existing_user = User.objects(email=data['email']).first()
+    if existing_user:
+        return jsonify({'message': 'User with this email already exists'}), 409
+
+    hashed_password = bcrypt.generate_password_hash(
+        data['password']).decode('utf-8')
+    new_user = User(
+        username=data['username'], email=data['email'], password=hashed_password)
+    new_user.save()
+
+    # Convert new_user to a dictionary before returning as a JSON response
+    user_data = new_user.to_mongo().to_dict()
+
+    # Convert ObjectId to string
+    user_data['_id'] = str(user_data['_id'])
+
+    return jsonify(message="successfully register", user=user_data)
+
+
+# User login endpoint
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.objects(email=data['email']).first()
+    if not user or not bcrypt.check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid email or password'}), 401
+
+    # Convert ObjectId to string before returning the JSON response
+    user_dict = user.to_mongo()
+    user_dict['_id'] = str(user_dict['_id'])
+
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify(user=user_dict, access_token=access_token)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
