257833489494fdb8ec8c8104cb88d52bf696b7b8
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-07-23 19:05:16 +0530

    final SUbmit_03

--- Project/MovieMania/src/environments/environment.ts
+++ Project/MovieMania/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiKey: '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',
+    apiKey: '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',
   };
   
